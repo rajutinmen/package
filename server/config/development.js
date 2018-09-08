1cd4e9f18935b250c79c9c5c268998667a2bc3b3
joker0575 <raju.msit05@gmail.com>
2018-09-08 23:52:18 +0530

    added db url

--- server/config/development.js
+++ server/config/development.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: "3000",
 	mongodb: {
-		url: 'mongodb://localhost:27017/packaging',
+		url: 'mongodb://heroku_7qsnwmb9:ag5kfal0j3sumln94g3pk3kipf@ds139501-a0.mlab.com:39501,ds139501-a1.mlab.com:39501/heroku_7qsnwmb9?replicaSet=rs-ds139501',
 		username: "tinmen",
 		password: "tinmen@1234"
 	},
